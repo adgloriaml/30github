eba463ae7e0b08796f5534201637c7acd23848d4
adgloriaml <dzhangaliev9@gmail.com>
2025-02-12 18:27:29 +0200

    arithmetic operations

---
+++ c_getting_started/week2/arithmetic.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Dear Procrastinator,\n");
+    printf("You still have to wait for %d days (%d minutes or %d seconds) before you can procrastinate!", 25 - 23, 60 * 24 * 2, 60 * 60 * 24 * 2);
+    return 0; 
+}
