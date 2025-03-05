d36a56f828cff53b584ce98523cfcba8e18e32f6
adgloriaml <dzhangaliev9@gmail.com>
2025-03-05 17:15:48 +0200

    uploaded the 2nd week exercises

--- c_modular/week2/3.c
+++ c_modular/week2/3.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
-
 int main(void) {
-	for(int i=0; i<10; i++) {
-		printf("%d", i);
+	char letter;
+	int num;
+	scanf("%c %d", &letter, &num);
+	int total = 0;
+	if(letter == 'i'){
+		total = sizeof(int)*num;
+        }
+	else if(letter == 'd'){
+                total = sizeof(double)*num;
 	}
-	return 0;
+	else if(letter == 's'){
+		total = sizeof(short)*num;
+	}
+	else if(letter == 'c'){
+		total = sizeof(char)*num;
+	}
+	
+	
+	int mb = total / (1000 * 1000);
+        int kb = (total % (1000 * 1000)) / 1000;
+        int b = total % 1000;
+
+
+	if (mb > 0) {
+		printf("%d MB", mb);
+		if (kb > 0 || b > 0) {
+			printf(" and ");
+		}
+	}
+
+	if (kb > 0) {
+		printf("%d KB", kb);
+		if (b >= 0) {
+			printf(" and ");
+		}
+	}
+
+	if (b >= 0) {
+		printf("%d B", b);
+	}
+        printf("\n");
 }
